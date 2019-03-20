bc2a2c254a685d63f6e927639fdb225fd3041b96
Resham Arora <resham.arora@gmail.com>
2019-03-20 01:39:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,15 +17,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
-  return "I can't hear you!"
-}
+  {return "I can't hear you!"}
 
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string)
-  return "YES INDEED!"
-}
-
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.")
-  return "I love you, too."
+  else if (string.toUpperCase() === string)
+  {return "YES INDEED!"}
+  
+  else if (string === "I love you, Grandma.")
+  {return "I love you, too."}
 }
